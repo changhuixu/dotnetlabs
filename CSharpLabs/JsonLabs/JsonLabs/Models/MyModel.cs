d59c931b4247ce6700ed4c8c108d5ee6cf378e89
Changhui Xu <changhuixu@hotmail.com>
2019-10-21 16:00:53 -0500

    add JSON labs for new System.Text.Json usages

---
+++ CSharpLabs/JsonLabs/JsonLabs/Models/MyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonLabs.Models
+{
+    public class MyModel
+    {
+        public string MyString { get; set; }
+        public int MyInt { get; set; }
+        public bool MyBoolean { get; set; }
+        public decimal MyDecimal { get; set; }
+        public DateTime MyDateTime1 { get; set; }
+        public DateTime MyDateTime2 { get; set; }
+        public List<string> MyStringList { get; set; }
+        public Dictionary<string, Person> MyDictionary { get; set; }
+        public MyModel MyAnotherModel { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
