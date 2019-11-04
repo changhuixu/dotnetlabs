608ed2e6a0564c44024ece2aa609fa615ae71873
Changhui Xu <changhuixu@hotmail.com>
2019-11-04 14:23:19 -0600

    add worker service, add logging, add FileSystemWatcher

---
+++ ASPNetCoreLabs/WindowsServiceDemo/Demo/Worker.cs
@@ -0,0 +1,73 @@
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Demo
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private FileSystemWatcher _folderWatcher;
+        private readonly string _inputFolder;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+            _inputFolder = @"C:\temp";
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.CompletedTask;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service Starting");
+            if (!Directory.Exists(_inputFolder))
+            {
+                _logger.LogWarning($"Please make sure the InputFolder [{_inputFolder}] exists, then restart the service.");
+                return Task.CompletedTask;
+            }
+
+            _logger.LogInformation($"Binding Events from Input Folder: {_inputFolder}");
+            _folderWatcher = new FileSystemWatcher(_inputFolder, "*.TXT")
+            {
+                NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.FileName |
+                                  NotifyFilters.DirectoryName
+            };
+            _folderWatcher.Created += Input_OnChanged;
+            _folderWatcher.EnableRaisingEvents = true;
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected void Input_OnChanged(object source, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Created)
+            {
+                _logger.LogInformation($"InBound Change Event Triggered by [{e.FullPath}]");
+
+                // do some work
+
+                _logger.LogInformation("Done with Inbound Change Event");
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping Service");
+            _folderWatcher.EnableRaisingEvents = false;
+            await base.StopAsync(cancellationToken);
+        }
+
+        public override void Dispose()
+        {
+            _logger.LogInformation("Disposing Service");
+            _folderWatcher.Dispose();
+            base.Dispose();
+        }
+    }
+}
