22b16a75de58a84885f3d2a48214e81c7bfd1c21
Unknown <changhuixu@hotmail.com>
2019-11-27 20:03:11 -0600

    fix Docker file; fix js file

--- MiscLabs/MachineLearning/HandwritingRecognition/wwwroot/js/site.js
+++ MiscLabs/MachineLearning/HandwritingRecognition/wwwroot/js/site.js
@@ -4,70 +4,81 @@
 // Write your JavaScript code.
 
 (function() {
-    var mousePressed = false;
-    var lastX, lastY;
-    var ctx = document.getElementById("canvas").getContext("2d");
+  var mousePressed = false;
+  var lastX, lastY;
+  var el = document.getElementById('canvas');
+  if (!el) return;
+  var ctx = el.getContext('2d');
 
-    $("#canvas").mousedown(function(e) {
-        mousePressed = true;
-        draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-    });
+  $('#canvas').mousedown(function(e) {
+    mousePressed = true;
+    draw(
+      e.pageX - $(this).offset().left,
+      e.pageY - $(this).offset().top,
+      false
+    );
+  });
 
-    $("#canvas").mousemove(function(e) {
-        if (mousePressed) {
-            draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-        }
-    });
-
-    $("#canvas").mouseup(function() {
-        mousePressed = false;
-    });
-    $("#canvas").mouseleave(function() {
-        mousePressed = false;
-    });
-
-    $("#clearArea").click(function() {
-        clearArea();
-    });
-
-    $("#check").click(function() {
-        check();
-    });
-
-    function draw(x, y, isDown) {
-        if (isDown) {
-            ctx.beginPath();
-            ctx.strokeStyle = "#FF0000";
-            ctx.lineWidth = "32";
-            ctx.lineJoin = "round";
-            ctx.moveTo(lastX, lastY);
-            ctx.lineTo(x, y);
-            ctx.closePath();
-            ctx.stroke();
-        }
-        lastX = x; lastY = y;
+  $('#canvas').mousemove(function(e) {
+    if (mousePressed) {
+      draw(
+        e.pageX - $(this).offset().left,
+        e.pageY - $(this).offset().top,
+        true
+      );
     }
+  });
 
-    function clearArea() {
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        $("#prediction").text("?");
-        $("#scores").text("");
-    }
+  $('#canvas').mouseup(function() {
+    mousePressed = false;
+  });
+  $('#canvas').mouseleave(function() {
+    mousePressed = false;
+  });
 
-    function check() {
-        var canvas = document.getElementById("canvas");
-        $("#prediction").text("?");
-        $.ajax({
-            type: "POST",
-            url: "home/upload",
-            data: {
-                imgBase64: canvas.toDataURL()
-            }
-        }).done(function(msg) {
-            console.log(msg.pixelValues);
-            $("#prediction").text(msg.prediction);
-            $("#scores").text(msg.scores);
-        });
+  $('#clearArea').click(function() {
+    clearArea();
+  });
+
+  $('#check').click(function() {
+    check();
+  });
+
+  function draw(x, y, isDown) {
+    if (isDown) {
+      ctx.beginPath();
+      ctx.strokeStyle = '#FF0000';
+      ctx.lineWidth = '32';
+      ctx.lineJoin = 'round';
+      ctx.moveTo(lastX, lastY);
+      ctx.lineTo(x, y);
+      ctx.closePath();
+      ctx.stroke();
     }
-}());
+    lastX = x;
+    lastY = y;
+  }
+
+  function clearArea() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    $('#prediction').text('?');
+    $('#scores').text('');
+  }
+
+  function check() {
+    var canvas = document.getElementById('canvas');
+    $('#prediction').text('?');
+    $.ajax({
+      type: 'POST',
+      url: 'home/upload',
+      data: {
+        imgBase64: canvas.toDataURL()
+      }
+    }).done(function(msg) {
+      console.log(msg.pixelValues);
+      $('#prediction').text(msg.prediction);
+      $('#scores').text(msg.scores);
+    });
+  }
+})();
