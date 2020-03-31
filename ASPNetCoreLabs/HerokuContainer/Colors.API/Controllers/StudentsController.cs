2d4b466581ad7b1d140e69bb061fbc256f8fe2cd
Changhui Xu <changhuixu@hotmail.com>
2020-03-31 16:57:57 -0500

    add other API examples for Swagger

---
+++ ASPNetCoreLabs/HerokuContainer/Colors.API/Controllers/StudentsController.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Colors.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly ILogger<StudentsController> _logger;
+
+        public StudentsController(ILogger<StudentsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// View a form
+        /// </summary>
+        /// <param name="id">Student ID</param>
+        /// <param name="formId">Form ID</param>
+        /// <returns></returns>
+        [HttpGet("{id:int}/forms/{formId:int}")]
+        [ProducesResponseType(typeof(FormSubmissionResult), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<FormSubmissionResult> ViewForm(int id, int formId)
+        {
+            _logger.LogInformation($"viewing the form#{formId} for Student ID={id}");
+            await Task.Delay(1000);
+            return new FormSubmissionResult { FormId = formId, StudentId = id };
+        }
+
+        /// <summary>
+        /// Submit a form which contains a key-value pair and a file.
+        /// </summary>
+        /// <param name="id">Student ID</param>
+        /// <param name="form">A form which contains the FormId and a file</param>
+        /// <returns></returns>
+        [HttpPost("{id:int}/forms")]
+        [ProducesResponseType(typeof(FormSubmissionResult), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<FormSubmissionResult>> SubmitForm(int id, [FromForm] StudentForm form)
+        {
+            _logger.LogInformation($"validating the form#{form.FormId} for Student ID={id}");
+            _logger.LogInformation($"saving file [{form.StudentFile.FileName}]");
+            await Task.Delay(1500);
+            _logger.LogInformation("file saved.");
+            var result = new FormSubmissionResult { FormId = form.FormId, StudentId = id };
+            return CreatedAtAction(nameof(ViewForm), new { id, form.FormId }, result);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpDelete("{id:int}/forms/{formId:int}")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<bool> Delete(int id, int formId)
+        {
+            _logger.LogInformation("This API is hidden in Swagger UI and Swagger JSON doc.");
+            _logger.LogInformation($"deleting the form#{formId} for student ID=[{id}]");
+            await Task.Delay(1500);
+            return true;
+        }
+    }
+
+    public class StudentForm
+    {
+        [Required] public int FormId { get; set; }
+        [Required] public IFormFile StudentFile { get; set; }
+    }
+
+    public class FormSubmissionResult
+    {
+        public int StudentId { get; set; }
+        public int FormId { get; set; }
+    }
+}
--- ASPNetCoreLabs/HerokuContainer/README.md
+++ ASPNetCoreLabs/HerokuContainer/README.md
@@ -1,9 +1,15 @@
-# Deploy a Dockerized ASP.NET Core 3 Web API app to Heroku
+# Dockerized ASP.NET Core 3 Web API app in Heroku
+
+## [Medium Article 1: Deploy a Dockerized ASP.NET Core 3 Web API app to Heroku](https://codeburst.io/deploy-a-containerized-asp-net-core-app-to-heroku-using-github-actions-9e54c72db943)
+
+In this blog post, we will create a containerized ASP.NET Core 3.1 Web API project, and set up a CI/CD pipeline using GitHub Actions. In the GitHub workflow, we will build and test the Web API project, and deploy the final docker image to Heroku.
+
+## [Medium Article 2: File Upload via Swagger](https://codeburst.io/file-uploading-in-swagger-e6c21b54d036)
+
+In this article, we will go over examples about uploading a single file, uploading a list of files, and uploading a file in a FormData object.
+
+---
 
 ## [API Website](https://icolors.herokuapp.com)
 
 <img src="./swagger-ui.png" width="650">
-
-## [Medium Article](https://codeburst.io/deploy-a-containerized-asp-net-core-app-to-heroku-using-github-actions-9e54c72db943)
-
-In this blog post, we will create a containerized ASP.NET Core 3.1 Web API project, and set up a CI/CD pipeline using GitHub Actions. In the GitHub workflow, we will build and test the Web API project, and deploy the final docker image to Heroku.
