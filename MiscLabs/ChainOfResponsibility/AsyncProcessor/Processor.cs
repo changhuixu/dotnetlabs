e74dee1ba49cdc1c761160079eeb4c4a92ac618d
Changhui Xu <changhuixu@hotmail.com>
2020-05-08 00:35:00 -0500

    ChainOfResponsibility Example: bank approval process

---
+++ MiscLabs/ChainOfResponsibility/AsyncProcessor/Processor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncProcessor
+{
+    public class Processor
+    {
+
+    }
+
+    public class AppContext
+    {
+
+    }
+    public static class UseExtensions
+    {
+        //public static Processor Use(this Processor processor, Func<AppContext, Func<Task>, Task> middleware)
+        //{
+        //    return processor.Use(next =>
+        //    {
+        //        return context =>
+        //        {
+        //            Func<Task> simpleNext = () => next(context);
+        //            return middleware(context, simpleNext);
+        //        };
+        //    });
+        //}
+    }
+}
---
+++ MiscLabs/ChainOfResponsibility/TransactionApproval/Program.cs
@@ -0,0 +1,24 @@
+﻿using TransactionApproval.Models;
+
+namespace TransactionApproval
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var bankManager = new BankManager("Bob");
+            var bankSupervisor = new Supervisor("Smith");
+            bankSupervisor.SetNextUpLevel(bankManager);
+            var frontLineStaff = new Teller("Taylor");
+            frontLineStaff.SetNextUpLevel(bankSupervisor);
+
+            var account = new BankAccount();
+            account.WithdrawMoney(frontLineStaff, 5000);
+            account.WithdrawMoney(frontLineStaff, 50000);
+            account.WithdrawMoney(frontLineStaff, 500000);
+
+            var teller2 = new Teller("Tim");
+            account.WithdrawMoney(teller2, 50000);
+        }
+    }
+}
