8b37b5a150a6ae6bda36f4dbb97e21cb78a89bee
Changhui Xu <changhuixu@hotmail.com>
2020-05-08 13:42:52 -0500

    add SQL server example

---
+++ EntityFrameworkLabs/StringOperations/StringOperations/DbContext/MySqlServerDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StringOperations.Models;
+
+namespace StringOperations.DbContext
+{
+    public class MySqlServerDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Customer> Customers { get; protected set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder = optionsBuilder
+                .UseLoggerFactory(Program.MyLoggerFactory)
+                .UseSqlServer("Server=.;Database=TestDb;Trusted_Connection=True;Integrated Security=True;MultipleActiveResultSets=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+            modelBuilder.Entity<Customer>().HasKey(x => x.Uuid);
+            modelBuilder.Entity<Customer>().Property(x => x.Uuid).ValueGeneratedNever();
+        }
+    }
+}
---
+++ EntityFrameworkLabs/StringOperations/StringOperations/Migrations/MySqlServerDb/20200508152252_Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StringOperations.Migrations.MySqlServerDb
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Uuid = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Dob = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<string>(nullable: true),
+                    Seniority = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Uuid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
